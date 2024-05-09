b8f8101ecf3c842df8137c90da2ea2551a3384c6
NancyBatham <nancy751batham@gmail.com>
2024-05-09 21:04:13 +0530

    adding todos in localstorage

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
-import Header from './components/Header';
-import {AddTodo} from './components/AddTodo';
-import Footer from './components/Footer';
-import Todos from './components/Todos';
-import React,{useState} from 'react';
+import Header from "./components/Header";
+import { AddTodo } from "./components/AddTodo";
+import Footer from "./components/Footer";
+import Todos from "./components/Todos";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  let initTodo;
 
-  const onDelete=(mytodo)=>{
-    setTodos(todolist.filter((e)=>{
-     return e!==mytodo
-    }))
+  if (localStorage.getItem("todolist") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todolist"));
   }
-  const addTodo =(title,desc)=>{
-   let sno;
-   if(todolist.length===0){
-    sno = 1;
-   }else{sno = todolist[todolist.length-1].sno + 1;}
-   
-   const mytodo={
-    sno: sno,
-    title: title,
-    desc: desc
-   }
 
-   setTodos([...todolist, mytodo]);
-  alert('Todo added successfully');
- 
+  const onDelete = (mytodo) => {
+    setTodos(
+      todolist.filter((e) => {
+        return e !== mytodo;
+      })
+    );
+  };
 
-  }
+  const addTodo = (title, desc) => {
+    let sno ;
+//console.log(todolist.length);
+    if (todolist.length === 0) {
+    //  console.log('i m 0');
+      sno = 1;
+    } else {
+    //  console.log('i m not 0');
+      //console.log(todolist);
+      sno = todolist[todolist.length - 1].sno + 1;
+    }
+
+    const mytodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    };
+    //console.log(mytodo);
+    setTodos([...todolist, mytodo]);
+    alert("Todo added successfully");
+    localStorage.setItem("todolist", JSON.stringify(todolist));
   
-  
-  let [todolist, setTodos] = useState([
-    {
-      sno:1,
-      title:'title1',
-      desc:'desc1',
+  };
 
-    },
-    {
-      sno:2,
-      title:'title2',
-      desc:'desc2',
+  let [todolist, setTodos] = useState(initTodo);
 
-    }, 
-    {
-      sno:3,
-      title:'title3',
-      desc:'desc3',
-
-    },
-    
-  ]);
-
+  // useEffect(() => {
+  //   localStorage.setItem("todolist", JSON.stringify(todolist));
+  // }, [todolist]);
   return (
     <>
-      <Header logo='MyReact'  />
+      <Header logo="MyReact" />
       <AddTodo addTodo={addTodo} />
       <Todos todo={todolist} onDelete={onDelete} />
       <Footer />
-    </> 
+    </>
   );
 }
 
